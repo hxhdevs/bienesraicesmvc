4d568aa63f30dd86e031043ef5ed35990f91645f
HXHdevs <jordisonnicolas@gmail.com>
2025-01-30 23:28:52 -0600

    Creando los modelos y actualizando con composer updated checar porque en README

---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => 'tetos/bienesraices',
+        'pretty_version' => 'dev-mmainmvc',
+        'version' => 'dev-mmainmvc',
+        'reference' => '4af113ff55bb4cd95d90b7fb0babf70dbbf5badf',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'intervention/gif' => array(
+            'pretty_version' => '4.2.1',
+            'version' => '4.2.1.0',
+            'reference' => '6addac2c68b4bc0e37d0d3ccedda57eb84729c49',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../intervention/gif',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'intervention/image' => array(
+            'pretty_version' => '3.11.0',
+            'version' => '3.11.0.0',
+            'reference' => '6b9ce4fc4485d30117e13935b25bc55a8b894a79',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../intervention/image',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'tetos/bienesraices' => array(
+            'pretty_version' => 'dev-mmainmvc',
+            'version' => 'dev-mmainmvc',
+            'reference' => '4af113ff55bb4cd95d90b7fb0babf70dbbf5badf',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
