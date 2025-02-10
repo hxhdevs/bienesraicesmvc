b2bd335599aff8eae4c9a12dee78f15d6237e289
HXHdevs <jordisonnicolas@gmail.com>
2025-02-10 00:46:21 -0600

    Probando rutas del controlador de Vendedores

--- controllers/VendedoresController.php
+++ controllers/VendedoresController.php
@@ -8,18 +8,18 @@ use MVC\Router;
 class VendedoresController{
 
     public static function index(){
-       
+       echo 'index vendedor';
     }
 
     public static function crear(){
-
+        echo 'Creando vendedor';
     }
     
     public static function actualizar(){
-        
+        echo 'Actualizando vendedor';
     }
 
     public static function eliminar(){
-       
+        echo 'Eliminando vendedor';
     }
 }
