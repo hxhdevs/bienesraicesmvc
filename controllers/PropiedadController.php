576a625c98c256d2b2624ae3672fdc10af1e8682
HXHdevs <jordisonnicolas@gmail.com>
2025-02-10 00:14:04 -0600

    Eliminando propiedades

--- controllers/PropiedadController.php
+++ controllers/PropiedadController.php
@@ -86,4 +86,19 @@ class PropiedadController{
             }
         }
     }
+
+    public static function eliminar(){
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            dep($_POST);
+            $id = $_POST['id'];
+            $id = filter_var($id, FILTER_VALIDATE_INT);
+            if ($id) {
+                $tipo = $_POST['tipo'];
+                if (validarTipoContenido($tipo)) {
+                    $propiedad = Propiedad::find($id);
+                    $propiedad->eliminar();
+                }                
+            }
+        }
+    }
 }
