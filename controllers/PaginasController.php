c6d35caafa2c2aed19ed8515d2430305caf243cd
HXHdevs <jordisonnicolas@gmail.com>
2025-02-12 23:42:27 -0600

    Agregando la pagina principal

--- controllers/PaginasController.php
+++ controllers/PaginasController.php
@@ -2,11 +2,19 @@
 
 namespace Controllers;
 
+use Model\Propiedad;
+use MVC\Router;
 
 class PaginasController{
 
-    public static function index(){
-       echo 'index vendedor';
+    public static function index(Router $router){
+      $propiedades = Propiedad::get(3);
+      $inicio = true;
+
+       $router->render('paginas/index',[
+            'propiedades' => $propiedades,
+            'inicio' => $inicio
+       ]);
     }
 
     public static function nosotros(){
---
+++ views/paginas/index.php
@@ -0,0 +1,88 @@
+
+    <main class="contenedor seccion">
+        <h1>Mas sobre nosotros</h1>
+        <div class="iconos-nosotros">
+            <div class="icono">
+                <img src="build/img/icono1.svg" alt="icono seguridad" loading="lazy">
+                <h3>Seguridad</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis veniam optio possimus quae blanditiis deserunt vel doloribus molestias voluptatum tempora!</p>
+            </div>
+            <div class="icono">
+                <img src="build/img/icono2.svg" alt="icono Precio" loading="lazy">
+                <h3>Precio</h3>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat ipsam rem magnam, quas non ratione officiis ea esse magni obcaecati, vel cumque aspernatur deleniti!</p>
+            </div>
+            <div class="icono">
+                <img src="build/img/icono3.svg" alt="icono seguridad" loading="lazy">
+                <h3>Seguridad</h3>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat ipsam rem magnam, quas non ratione officiis ea esse magni obcaecati, vel cumque aspernatur deleniti!</p>
+            </div>
+        </div>
+    </main>
+
+<?php
+    include 'listado.php';
+?>
+
+    <section class="imagen-contacto">
+        <h2>Encuentra la casa de tus sueños</h2>
+        <p>LLena el formulario de contacto y un asesor se pondra en contacto contigo</p>
+        <a href="contacto.html" class="boton-amarillo">Contactanos</a>
+    </section>
+
+    <div class="contenedor seccion seccion-inferior">
+        <section class="blog">
+            <h3>Nuestro blog</h3>
+
+            <article class="entrada-blog">
+                <div class="imagen">
+                    <picture>
+                        <source srcset="build/img/blog1.webp" type="image/webp">
+                        <source srcset="build/img/blog1.jpg" type="image/jpeg">
+                        <img loading="lazy" src="build/img/blog1.jpg" alt="Texto entrada blog">
+                    </picture>
+                </div>
+
+                <div class="texto-entrada">
+                    <a href="entrada.html">
+                        <h4>Terraza en el techo de tu casa</h4>
+                        <p class="informacion-meta">Escrito el:<span>10/09/2024</span> por: <span>Admin</span></p>
+                        <p>
+                            Consejos para construir una terraza en el techo de tu casa con los mejores materiales y ahorrando dinero
+                        </p>
+                    </a>
+                </div>
+            </article>
+            
+            <article class="entrada-blog">
+                <div class="imagen">
+                    <picture>
+                        <source srcset="build/img/blog2.webp" type="image/webp">
+                        <source srcset="build/img/blog2.jpg" type="image/jpeg">
+                        <img loading="lazy" src="build/img/blog1.jpg" alt="Texto entrada blog">
+                    </picture>
+                </div>
+
+                <div class="texto-entrada">
+                    <a href="entrada.html">
+                        <h4>Guia para la decoracion de tu hogar</h4>
+                        <p class="informacion-meta">Escrito el:<span>10/09/2024</span> por: <span>Admin</span></p>
+                        <p>
+                            Maximiza el espacio en tu hogar con esta guia, aprende a combinar muebles y colores para darle vida a tu espacio
+                        </p>
+                    </a>
+                </div>
+            </article>
+        </section>
+
+        <section class="testimoniales">
+            <h3>Testimoniales</h3>
+
+            <div class="testimonial">
+                <blockquote>
+                    El personal se comporto de una excelente forma, muy buena atencion y la casa que me ofrecieron cumple con todas mis espectativas.
+                </blockquote>
+                <p>- HxH Devs</p>
+            </div>
+        </section>
+    </div>
