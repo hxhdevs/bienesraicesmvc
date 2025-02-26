e16a2c3818693a03af394f20a010f920cfce00b0
HXHdevs <jordisonnicolas@gmail.com>
2025-02-25 23:49:42 -0600

    Creando el modelo de autenticacion

--- models/Admin.php
+++ models/Admin.php
@@ -2,5 +2,17 @@
 namespace Model;
 
 class Admin extends ActiveRecord{
+    protected static $tabla ='usuarios';   
+    protected static $columnasDB =['id','email','password'];
     
+    public $id;
+    public $email;
+    public $password;
+
+    public function __construct($args =[])
+    {
+        $this->id = $args['id']?? null;
+        $this->email = $args['email']?? null;
+        $this->password = $args['password']?? null;
+    }
 }
